67a76fa49fc1f32c1fb0d655a5023272b73a1271
prathameshthorat <thoratprathamesh@yahoo.com>
2020-08-03 22:49:14 +0530

    Deployment changes

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const app = express();
+const port = process.env.PORT || 8080;
+
 // Run the app by serving the static files
 // in the dist directory
 app.use(express.static(__dirname + '/dist/TaskManager'));
@@ -8,4 +10,7 @@ app.use(express.static(__dirname + '/dist/TaskManager'));
 app.get('/', (req,res) => {
     res.sendFile(path.join(__dirname+'/dist/TaskManager/index.html'));
   });
-app.listen(process.env.PORT || 8080);
+
+app.listen(port, () => {
+    console.log(`Listening at http://localhost:${port}`)
+});
