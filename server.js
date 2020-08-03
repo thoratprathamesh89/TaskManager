0070837c1e9bf07232c29e7174fd7076b4c74336
prathameshthorat <thoratprathamesh@yahoo.com>
2020-08-03 22:04:06 +0530

    deployment Update

--- server.js
+++ server.js
@@ -5,4 +5,7 @@ const app = express();
 app.use(express.static(__dirname + '/dist'));
 // Start the app by listening on the default
 // Heroku port
+app.get('/', (req,res) => {
+    res.sendFile(process.cwd()+"/TaskManager/dist/TaskManager/index.html");
+  });
 app.listen(process.env.PORT || 8080);
