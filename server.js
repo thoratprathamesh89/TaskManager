0cfbfdd28e8893de79a1e9634a40b5780cb152e5
prathameshthorat <thoratprathamesh@yahoo.com>
2020-08-03 22:30:39 +0530

    Deployment Changes

--- server.js
+++ server.js
@@ -2,10 +2,10 @@ const express = require('express');
 const app = express();
 // Run the app by serving the static files
 // in the dist directory
-app.use(express.static(__dirname + '/dist'));
+app.use(express.static(__dirname + '/dist/TaskManager'));
 // Start the app by listening on the default
 // Heroku port
 app.get('/', (req,res) => {
-    res.sendFile(process.cwd()+"/TaskManager/dist/TaskManager/index.html");
+    res.sendFile(path.join(__dirname+'/dist/TaskManager/index.html'));
   });
 app.listen(process.env.PORT || 8080);
